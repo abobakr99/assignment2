c8c138a5304aadcd1ca7c4a3ee33973d5b34122b
huSin <hussainaljabri@cmail.carleton.ca>
2016-10-26 16:21:14 -0700

    Update part2.c

--- part2.c
+++ part2.c
@@ -6,7 +6,11 @@
 #define _GNU_SOURCE
 #include <stdbool.h>
 #define MAX 100
+#include <unistd.h>
 
+
+
+//-----------------------------------------------------------------------//
 
 
 struct Process{
@@ -15,13 +19,23 @@ struct Process{
 	int CpuTime;
 	int IOfrequency;
 	int IOduration;
+	char *oldState;
+	char *newState;
+	int waitingTime;
+	int turnaroundTime;
 	
 };
-
+//-----------------------------------------------------------------------//
+						// Global Variables.
+//-----------------------------------------------------------------------//
 int arrayIndex; // also refers to how many elements in the Array (Processes in array).
 struct Process procArray[MAX]; // global variable so i dont have to pass them into parameter.
 FILE *ofp;
-
+char waiting[] = "Waiting State.";
+char ready[] = "Ready State.";
+char running[] = "Running State.";
+char new[] = "New State.";
+char terminated[] = "Terminated State.";
 
 void appendProcess(struct Process proc ){
 	if(arrayIndex != MAX-1){
@@ -30,6 +44,17 @@ void appendProcess(struct Process proc ){
 	}else{
 		printf("Process Queue is Full.");
 	}
+}
+
+
+void removeFirst(){
+	for(int i = 0; i<arrayIndex; i++) procArray[i] = procArray[i+1];
+
+	arrayIndex--;
+}
+
+void sleep(unsigned milliseconds){
+	//usleep(milliseconds*1000);
 }
 
 void inputFileReader(void){
@@ -47,6 +72,9 @@ void inputFileReader(void){
 		temp.CpuTime = exe;
 		temp.IOduration = 1;
 		temp.IOfrequency = 1;
+		temp.oldState = new;
+		temp.newState = ready;
+		temp.turnaroundTime = temp.waitingTime = 0;
 		//---------sending temp to appendProcess---------//
 		appendProcess(temp);
 		
@@ -63,31 +91,77 @@ void out(char str[]){
 	
 }
 
+
+void print_table(struct Process p[]){
+    int i;
+
+    puts("+-----+---------------------+------------------+--------------------+");
+    puts("| PID |  Time of transition |    Old State     |     New State      |");
+    puts("+-----+---------------------+------------------+--------------------+");
+
+    for(i=0; i<arrayIndex; i++) {
+        printf("| %2d  |         %2d          |    %s    |    %s    |\n"
+               , p[i].pid, p[i].waitingTime , p[i].oldState, p[i].newState );
+        puts("+-----+---------------------+------------------+--------------------+");
+    }
+	printf("\n");
+
+}
+
+void procSimulator(){
+	
+	// int i here refers on the time in millisconds.
+	for(int i = 0;i<arrayIndex; i++){ // means theres no more process in ready state , break.
+
+		procArray[i].oldState = procArray[i].newState;
+		procArray[i].newState = running;
+		
+		print_table(procArray);
+		for(int j =0; j<(procArray[i].CpuTime * 1000);j++)
+		
+
+		procArray[i].oldState = procArray[i].newState;
+		procArray[i].newState = terminated;
+
+		print_table(procArray);
+
+	}
+
+
+
+}
+
+
 int main(void){
 
 	// --------------- asking to read input file -------//
 		inputFileReader(); // after this all process are stored in global access procArray.
 		//checking the array.
 		printf("How many process read: %d\n", arrayIndex);
-		for(int i=0; i<arrayIndex; i++){
-			printf(" %d," , procArray[i].pid);
+		procArray[0].turnaroundTime = procArray[0].CpuTime;
+		for(int i=1; i<arrayIndex; i++){
+			procArray[i].waitingTime = procArray[i-1].waitingTime + procArray[i-1].CpuTime;
+			procArray[i].turnaroundTime = procArray[i].waitingTime + procArray[i].CpuTime;
 	    }
-		printf("\n");
-
+		
 	//---------------- prepare output file to be used frequently as "Log"------///
-		ofp = fopen("Output.txt", "w");
+	ofp = fopen("Output.txt", "w");
 	if(ofp == NULL){
 		printf("ERROR\n");
 		exit(1);
 	}
-	
+	//--------- the start of our proccess scheduling -----------//
+	// FCFS scenario.
+	// all process should be ready to run.
+		print_table(procArray);
+		procSimulator();
+
+
+
+
 	fclose(ofp);
 	return 1;
 }
-
-
-
-
 
 
 
